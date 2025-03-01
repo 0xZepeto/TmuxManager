bd1d287e024db4cdda3958ce4be24dd94213d030
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-03-02 04:01:41 +0700

    Update Chmod.sh

--- Chmod.sh
+++ Chmod.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-chmod +x ./*.sh
+
+git stash && git pull && git stash pop
+
+# Cek apakah ada file .sh sebelum menjalankan chmod
+if find . -type f -name "*.sh" | grep -q .; then
+    find . -type f -name "*.sh" -exec chmod +x {} \;
+    echo "Update selesai! Semua file .sh telah diberikan izin eksekusi."
+else
+    echo "Tidak ada file .sh yang ditemukan."
+fi
+
