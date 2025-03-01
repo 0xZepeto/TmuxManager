cfa720e6b810d23c403b852cd2105863213ce01c
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-03-02 04:47:32 +0700

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-# Warna kuning untuk output
 YELLOW='\033[1;33m'
 RESET='\033[0m'
 
-# Folder target
 FOLDER="/home/hg680p/TmuxManager"
-
-# Pindah ke folder TmuxManager
 cd "$FOLDER" || exit
-
-# Jalankan git stash dan git pull tanpa output yang mengganggu
 git stash -q 2>/dev/null
 git pull --quiet 2>/dev/null
-
-# Berikan izin eksekusi ke semua file .sh hanya dalam folder ini (bukan subfolder)
 chmod +x "$FOLDER"/*.sh 2>/dev/null
-
-# Pesan selesaiSuccessful
 echo -e "${YELLOW}Update Successful!${RESET}"
