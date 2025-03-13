a266d56ab84f350f1c75d74895e1d5176ec80425
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-03-14 01:09:34 +0700

    Create Check.sh

---
+++ Check.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Warna
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
+# File log
+LOGFILE="/home/hg680p/TmuxManager/Check.log"
+
+# Daftar STB dan modem yang sesuai
+declare -A STB_MODEM=(
+    ["STB1"]="STB 1"
+    ["STB2"]="STB 2"
+    ["STB3"]="STB 3"
+    ["STB4"]="STB 4"
+    ["STB5"]="STB 5"
+    ["STB6"]="STB 6"
+    ["STB7"]="STB 7"
+    ["STB8"]="STB 8"
+)
+
+# Ambil hostname STB saat ini
+HOSTNAME=$(hostname)
+
+# Cek apakah hostname ada dalam daftar STB_MODEM
+if [[ -z "${STB_MODEM[$HOSTNAME]}" ]]; then
+    echo -e "${RED}$(date) - Hostname tidak ditemukan dalam daftar STB. Keluar...${RESET}" | tee -a "$LOGFILE"
+    # Potong log file menjadi 25 baris terakhir
+    tail -n 25 "$LOGFILE" > "$LOGFILE.tmp" && mv "$LOGFILE.tmp" "$LOGFILE"
+    exit 1
+fi
+
+MODEM_NAME="${STB_MODEM[$HOSTNAME]}"
+
+# Tampilkan pesan monitoring
+echo -e "${YELLOW}$(date) - Monitoring WiFi { $HOSTNAME } modem { $MODEM_NAME }${RESET}" | tee -a "$LOGFILE"
+
+# Cek status koneksi WiFi menggunakan NetworkManager
+WIFI_STATUS=$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d':' -f2)
+
+# Jika nmcli tidak mendeteksi, gunakan iw sebagai cadangan
+if [[ -z "$WIFI_STATUS" ]]; then
+    iw dev wlan0 link | grep -q "Connected"
+    if [[ $? -eq 0 ]]; then
+        WIFI_STATUS="$MODEM_NAME"
+    else
+        WIFI_STATUS="Disconnected"
+    fi
+fi
+
+# Cek status koneksi
+if [[ "$WIFI_STATUS" == "Disconnected" ]]; then
+    echo -e "${RED}$(date) - Koneksi WiFi ke $MODEM_NAME terputus! Menjalankan AutoConnect.sh...${RESET}" | tee -a "$LOGFILE"
+
+    # Jalankan AutoConnect.sh
+    /home/hg680p/TmuxManager/AutoConnect.sh >> "$LOGFILE" 2>&1
+
+    echo -e "${GREEN}$(date) - AutoConnect.sh selesai dijalankan.${RESET}" | tee -a "$LOGFILE"
+else
+    echo -e "${GREEN}$(date) - Koneksi WiFi ke $MODEM_NAME stabil.${RESET}" | tee -a "$LOGFILE"
+fi
+
+# Potong log file menjadi 25 baris terakhir
+tail -n 25 "$LOGFILE" > "$LOGFILE.tmp" && mv "$LOGFILE.tmp" "$LOGFILE"
