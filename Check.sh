e6683db7d2122f66e45acb888b780f7a892fd048
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-03-14 01:28:50 +0700

    Update Check.sh

--- Check.sh
+++ Check.sh
@@ -3,40 +3,20 @@
 # Warna
 GREEN='\033[1;32m'
 RED='\033[1;31m'
-BLUE='\033[1;34m'
 YELLOW='\033[1;33m'
 RESET='\033[0m'
 
 # File log
-LOGFILE="/home/hg680p/TmuxManager/Check.log"
-
-# Daftar STB dan modem yang sesuai
-declare -A STB_MODEM=(
-    ["STB1"]="STB 1"
-    ["STB2"]="STB 2"
-    ["STB3"]="STB 3"
-    ["STB4"]="STB 4"
-    ["STB5"]="STB 5"
-    ["STB6"]="STB 6"
-    ["STB7"]="STB 7"
-    ["STB8"]="STB 8"
-)
+LOGFILE="/tmp/check.log"
 
 # Ambil hostname STB saat ini
 HOSTNAME=$(hostname)
 
-# Cek apakah hostname ada dalam daftar STB_MODEM
-if [[ -z "${STB_MODEM[$HOSTNAME]}" ]]; then
-    echo -e "${RED}$(date) - Hostname tidak ditemukan dalam daftar STB. Keluar...${RESET}" | tee -a "$LOGFILE"
-    # Potong log file menjadi 25 baris terakhir
-    tail -n 25 "$LOGFILE" > "$LOGFILE.tmp" && mv "$LOGFILE.tmp" "$LOGFILE"
-    exit 1
-fi
-
-MODEM_NAME="${STB_MODEM[$HOSTNAME]}"
+# Modem name berdasarkan hostname
+MODEM_NAME="STB ${HOSTNAME: -1}"  # Mengambil angka terakhir dari hostname (misal STB8 -> 8)
 
 # Tampilkan pesan monitoring
-echo -e "${YELLOW}$(date) - Monitoring WiFi { $HOSTNAME } modem { $MODEM_NAME }${RESET}" | tee -a "$LOGFILE"
+echo -e "${YELLOW}$(date '+%Y-%m-%d %H:%M:%S') - Monitoring WiFi { $HOSTNAME } modem { $MODEM_NAME }${RESET}" | tee -a "$LOGFILE"
 
 # Cek status koneksi WiFi menggunakan NetworkManager
 WIFI_STATUS=$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d':' -f2)
@@ -53,15 +33,12 @@ fi
 
 # Cek status koneksi
 if [[ "$WIFI_STATUS" == "Disconnected" ]]; then
-    echo -e "${RED}$(date) - Koneksi WiFi ke $MODEM_NAME terputus! Menjalankan AutoConnect.sh...${RESET}" | tee -a "$LOGFILE"
-
-    # Jalankan AutoConnect.sh
+    echo -e "${RED}$(date '+%Y-%m-%d %H:%M:%S') - WiFi terputus! Menjalankan AutoConnect.sh...${RESET}" | tee -a "$LOGFILE"
     /home/hg680p/TmuxManager/AutoConnect.sh >> "$LOGFILE" 2>&1
-
-    echo -e "${GREEN}$(date) - AutoConnect.sh selesai dijalankan.${RESET}" | tee -a "$LOGFILE"
+    echo -e "${GREEN}$(date '+%Y-%m-%d %H:%M:%S') - AutoConnect.sh selesai dijalankan.${RESET}" | tee -a "$LOGFILE"
 else
-    echo -e "${GREEN}$(date) - Koneksi WiFi ke $MODEM_NAME stabil.${RESET}" | tee -a "$LOGFILE"
+    echo -e "${GREEN}$(date '+%Y-%m-%d %H:%M:%S') - WiFi stabil.${RESET}" | tee -a "$LOGFILE"
 fi
 
-# Potong log file menjadi 25 baris terakhir
+# Batasi log hanya 25 baris terakhir
 tail -n 25 "$LOGFILE" > "$LOGFILE.tmp" && mv "$LOGFILE.tmp" "$LOGFILE"
